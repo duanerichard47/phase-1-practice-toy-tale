30b64294b801581ba935afe0499eaea526dd9774
Duane <duanerichard47@hotmail.com>
2023-01-22 15:01:25 -0500

    incomplete 1

--- src/index.js
+++ src/index.js
@@ -13,3 +13,98 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+/// Start of my code
+const toyCollection = document.querySelector('#toy-collection')
+const btnClass = document.getElementsByClassName("like-btn")
+const newToyBtn = document.querySelector('#new-toy-btn')
+const addToyForm = document.getElementsByClassName("add-toy-form")
+
+fetch("http://localhost:3000/toys")
+.then(response=>response.json())
+.then(response=>response.forEach(toy=>createCard(toy)))
+
+
+
+function createCard(toy) 
+{
+const newDiv= document.createElement('div')
+newDiv.className = "card"
+toyCollection.appendChild(newDiv)
+
+const newh2= document.createElement('h2')
+newh2.textContent = toy.name 
+newDiv.appendChild(newh2)
+
+const imgToy =document.createElement('img')
+console.log(toy.image)
+imgToy.src = toy.image
+
+imgToy.className ='toy-avatar'
+newDiv.appendChild(imgToy)
+
+
+const newP = document.createElement('p')
+newP.textContent = `${toy.likes} likes`
+newDiv.appendChild(newP)
+
+const newBtn = document.createElement('button')
+newBtn.className = 'like-btn'
+newBtn.id = toy.id
+newBtn.textContent = 'Like  ♥'
+newDiv.appendChild(newBtn)
+
+newBtn.addEventListener('click', event =>patchToy(event))
+}
+
+
+addToyForm.addEventListener('submit', event=> postToy(event))
+
+
+
+function postToy(event){
+  let newToyName = addToyForm[1].value
+  let newToyImg = addToyForm[2].value
+
+fetch("http://localhost:3000/toys",{
+  method:'POST',
+  headers:
+    {
+  "Content-Type": "application/json",        //header content
+  Accept: "application/json"                    //header content
+    },   
+
+body: JSON.stringify(
+  {
+  "name": newToyName ,  //"Jessie",                  //body content      console.log(addToyForm[1]) 
+  "image": newToyImg ,  //"https://vignette.wikia.nocookie.net/p__/images/8/88/Jessie_Toy_Story_3.png/revision/latest?cb=20161023024601&path-prefix=protagonist",
+                                                            //console.log(addToyForm[2]) 
+  "likes": 0                                       //body content
+  }
+) 
+                                    })  // end of fetch and 2nd parameter function
+
+}     //end of function postToy
+
+function patchToy(event) {
+  event.preventDefault();  
+let newNumberOfLikes = (parseInt(event.target.previousSibling.textContent))
+
+let toyUrl =  `http://localhost:3000/toys/${event.target.id}`  //or is it newBtn.id
+
+fetch(toyUrl,{
+  method:'PATCH',
+  headers:
+  {
+    "Content-Type": "application/json",  //header content
+    Accept: "application/json"            //header content
+  },
+  
+  body: JSON.stringify(
+    {
+    "likes": newNumberOfLikes += 1      //body content
+    }
+)
+                                    }) // end of fetch and 2nd parameter function
+                                                                 
+ } //end of function patchToy
+
